7c02a0fe45cb969e184c9f7eb2670dfd038111b7
Christian Navarro <cnavarr5@bruinmail.slcc.edu>
2018-02-16 20:09:47 -0700

    The buttons work

---
+++ Script/master.js
@@ -0,0 +1,17 @@
+function toggle() {
+    var x = document.getElementById("cook");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    } else {
+        x.style.display = "block";
+    }
+}
+
+function toggle2() {
+    var x = document.getElementById("cook2");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    } else {
+        x.style.display = "block";
+    }
+}
