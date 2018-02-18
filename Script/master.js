332d2e9deb7c397dcc55b25317b766882660ad3c
Christian Navarro <cnavarr5@bruinmail.slcc.edu>
2018-02-18 16:35:18 -0700

    Buttons are uniform, looks better.

--- Script/master.js
+++ Script/master.js
@@ -1,17 +1,17 @@
 function toggle() {
     var x = document.getElementById("cook");
-    if (x.style.display === "block") {
-        x.style.display = "none";
-    } else {
+    if (x.style.display === "none") {
         x.style.display = "block";
+    } else {
+        x.style.display = "none";
     }
 }
 
 function toggle2() {
     var x = document.getElementById("cook2");
-    if (x.style.display === "block") {
-        x.style.display = "none";
-    } else {
+    if (x.style.display === "none") {
         x.style.display = "block";
+    } else {
+        x.style.display = "none";
     }
 }
